3bde7bf10aa6679a1c244f1cdcaf96cab7c5ee39
TitanjrLin <titan@truelovelive.dev>
2023-12-03 04:24:58 +0800

    chore: modify vite config base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
+  base: '/cw-interview',
   plugins: [
     react(),
     eslintPlugin({
